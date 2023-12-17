f66f3fde348aeb7a2c3df0646a777a5eb78aa76f
coaben2 <coaben2@gmail.com>
2023-12-17 12:50:54 +0100

    maj

--- Scripts/sac.js
+++ Scripts/sac.js
@@ -1,22 +1,69 @@
+function getCharacterBags(characterName, apiKey) {
+    var apiUrl = "https://api.guildwars2.com/v2/characters/" + characterName + "?access_token=" + apiKey;
+
+    $.ajax({
+        type: "GET",
+        url: apiUrl,
+        dataType: "json",
+        cache: true,
+        timeout: 10000,
+        success: function(characterData, textStatus, jqXHR) {
+            if (characterData.bags) {
+                var bagsContainer = $("#" + characterName + " .bags-container");
+                bagsContainer.empty();
+
+                $.each(characterData.bags, function(index, bag) {
+                    var bagHtml = '<div class="bag">';
+                    bagHtml += '<div class="item" data-itemid="' + bag.id + '"><a href="' + bag.url + '" target="_blank"><img src="assets/social/gw2-logo.png"></a> <strong class="item-name"></strong></div>';
+                    
+                    if (bag.inventory) {
+                        $.each(bag.inventory, function() {
+                            if (this.id) {
+                                var itemHtml = '<a href="' + this.url + '" class="item" data-itemid="' + this.id + '" target="_blank">';
+                                itemHtml += '<img src="assets/img/gw2-logo.png">';
+                                this.count > 1 && (itemHtml += "<span>" + this.count + "</span>");
+                                itemHtml += "</a>";
+                                bagsContainer.append(itemHtml);
+                            } else {
+                                bagsContainer.append('<div class="item empty"></div>');
+                            }
+                        });
+                    }
+
+                    bagHtml += '</div>';
+                    bagsContainer.append(bagHtml);
+                });
+            } else {
+                console.log("Character bags not found for: " + characterName);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Error fetching character data:', errorThrown);
+        }
+    });
+}
 function displayDuplicateItems(duplicateItems) {
-    var duplicatesContainer = $("#duplicatesContainer");
+    var duplicatesTable = $(".duplicates.container tbody");
+
+    // Efface le contenu existant du tableau
+    duplicatesTable.empty();
 
     if (duplicateItems.length > 0) {
         $.each(duplicateItems, function(index, itemKey) {
-            var itemHtml = '<div class="item-duplicate">' +
-                            '<span>Objet en double: ' + itemKey + '</span>' +
-                            '</div>';
-            duplicatesContainer.append(itemHtml);
+            var rowHtml = '<tr>' +
+                          '<td>Objet en double: ' + itemKey + '</td>' +
+                          '</tr>';
+            duplicatesTable.append(rowHtml);
         });
     } else {
-        // Aucun objet en double
-        duplicatesContainer.append('<p>Aucun objet en double détecté.</p>');
+        var noDuplicatesRow = '<tr><td colspan="3">Aucun objet en double détecté.</td></tr>';
+        duplicatesTable.append(noDuplicatesRow);
     }
 }
+var apiKey = key;
+getDuplicateItems(characterName, apiKey);
+displayDuplicateItems(duplicateItems);
 
-$(document).ready(function () {
-    displayDuplicateItems(duplicateItems)
-});
 $(window).scroll(function () { lazyLoad() });
 var groupsJSON, categoriesJSON, accountAchievementsJSON, achievementsArray = [];
 $("body").on("change", "#achievementGroups", function () { getAchievementGroupCategories($(this).val()) }), $("body").on("change", "#achievementCategories", function () { getCategoryAchievements($(this).val()) }), $("body").on("click", ".jq-collapse-achievements", function () { return $(this).toggleClass("collapsed"), $(".achievement-container").toggleClass("collapsed"), !1 });
